7f0f1c78b0284c6b93360a61472c3d3a180f1de3
SonHoang <sonhoang.ithubt@gmail.com>
2022-10-16 10:58:03 +0700

    Move business logic to use case layer

--- BLMusic/Domain/UseCases/DownloadSongUseCase.swift
+++ BLMusic/Domain/UseCases/DownloadSongUseCase.swift
@@ -29,10 +29,74 @@ final class DownloadSongUseCaseImpl: DownloadSongUseCase {
         progressHander: @escaping (Double) -> Void,
         completionHandler: @escaping (Result<URL, Error>) -> Void
     ) -> Cancellable? {
+        let songDTO = SongMapper.map(song)
         return songRepository.downloadSong(
-            SongMapper.map(song),
+            songDTO,
             progressHandler: progressHander,
-            completionHandler: completionHandler
+            completionHandler: { [weak self] result in
+                guard let self = self else {
+                    return
+                }
+                switch result {
+                case .success(let tmpURL):
+                    self.saveAudioFileToCache(
+                        songDTO: songDTO,
+                        tmpURL: tmpURL,
+                        completion: completionHandler
+                    )
+                case .failure(let error):
+                    completionHandler(.failure(error))
+                }
+            }
+        )
+    }
+}
+
+// MARK: Private functions
+extension DownloadSongUseCaseImpl {
+    private func saveAudioFileToCache(
+        songDTO: SongDTO,
+        tmpURL: URL,
+        completion: @escaping (Result<URL, Error>) -> Void
+    ) {
+        songRepository.saveAudioFileToCache(
+            of: songDTO,
+            in: tmpURL,
+            completion: { [weak self] result in
+                guard let self = self else {
+                    return
+                }
+                
+                switch result {
+                case .success(let audioCacheURL):
+                    self.saveSongToPersistentStorage(
+                        songDTO,
+                        audioCacheURL: audioCacheURL,
+                        completion: completion
+                    )
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        )
+    }
+    
+    private func saveSongToPersistentStorage(
+        _ songDTO: SongDTO,
+        audioCacheURL: URL,
+        completion: @escaping (Result<URL, Error>) -> Void
+    ) {
+        songRepository.saveSongToPersistentStorage(
+            songDTO,
+            audioCacheURL: audioCacheURL,
+            completion: { result in
+                switch result {
+                case .success:
+                    completion(.success(audioCacheURL))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
         )
     }
 }
